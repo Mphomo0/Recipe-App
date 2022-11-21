3eb808f281f3f392895fbb1517c9c0617e949363
Mpho <Mpho>
2022-11-21 20:41:37 +0200

    added media query

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,7 +6,7 @@ const Category = () => {
   return (
     <div className='lg:flex items-center justify-center mt-10 mb-10 bg[#2a303c] sm:flex flex-cols-4 w-full'>
       <Link to={'/cuisine/Italian'}>
-     <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-blue-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2 sm:text-xs">
+     <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-blue-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2 sm:text-xs test">
       <FaPizzaSlice /> &nbsp;&nbsp;Italian
      </button>
      </Link>
