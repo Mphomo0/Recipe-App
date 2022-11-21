8a4ad791e45542fa3898afe44eac48740c2eaa66
Mpho <Mpho>
2022-11-21 22:01:32 +0200

    css change

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Category = () => {
   return (
-    <div className='lg:flex items-center justify-center mt-10 mb-10 bg[#2a303c] sm:flex-cols-4'>
+    <div className='justify-center items-center  lg:flex mt-10 mb-10 bg[#2a303c]'>
       <Link to={'/cuisine/Italian'}>
      <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-blue-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2 sm:text-xs">
       <FaPizzaSlice /> &nbsp;&nbsp;Italian
